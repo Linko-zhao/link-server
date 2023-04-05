84e85605b1bad095c1edc90f86c486728c9986bf
linhao_zhao <1258608694@qq.com>
2023-04-05 10:07:26 +0800

    2023-4-5

--- link/log.h
+++ link/log.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <memory>
+#include <list>
 
 namespace slyar {
 
@@ -83,8 +84,32 @@ public:
 
     Logger(const std::string& name = "root");
 
-    void 
+    void log(LogLevel::Level level, LogEvent::ptr event);
+    void debug(LogEvent::ptr event);
+    void info(LogEvent::ptr event);
+    void warn(LogEvent::ptr event);
+    void error(LogEvent::ptr event);
+    void fatal(LogEvent::ptr event);
+    
+    void addAppender(LogAppender::ptr appender);
+    void delAppender(LogAppender::ptr appender);
+    void clearAppenders();
+
+    LogLevel::Level getLevel() const {return m_level;}
+    void setLevel(LogLevel::Level val) {m_level = val;}
+    const std::string& getName() const {return m_name;}
+    void setFormatter(LogFormatter::ptr val);
+    void setFormatter(const std::string& val);
+    
+    LogFormatter::ptr getFormatter();
+
 private:
+    std::string m_name;
+    LogLevel::Level m_level; 
+
+    std::list<LogAppender::ptr> m_appenders;
+    LogFormatter::ptr m_formatter;
+    Logger::ptr m_root;
 };
 
 
